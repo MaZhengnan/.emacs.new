1364421d5209277c2cda97b7103939d4b3fb853e
mazhengnan <mzn836443465@163.com>
2025-02-08 18:49:51 +0800

    modified

--- config.el
+++ config.el
@@ -653,10 +653,10 @@
 (eval-after-load 'org-indent '(diminish 'org-indent-mode))
 
 (custom-set-faces
- '(org-level-1 ((t (:inherit outline-1 :height 1.2))))
- '(org-level-2 ((t (:inherit outline-2 :height 1.1))))
- '(org-level-3 ((t (:inherit outline-3 :height 1.1))))
- '(org-level-4 ((t (:inherit outline-4 :height 1.1))))
+ '(org-level-1 ((t (:inherit outline-1 :height 1.0))))
+ '(org-level-2 ((t (:inherit outline-2 :height 1.0))))
+ '(org-level-3 ((t (:inherit outline-3 :height 1.0))))
+ '(org-level-4 ((t (:inherit outline-4 :height 1.0))))
  '(org-level-5 ((t (:inherit outline-5 :height 1.0))))
  '(org-level-6 ((t (:inherit outline-5 :height 1.0))))
  '(org-level-7 ((t (:inherit outline-5 :height 1.0)))))
